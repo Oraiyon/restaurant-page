4d5204737d3c3d603f6d3411aba57999cf6fde14
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-02 16:38:37 +0800

    index.html mimics ../src/template.html

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,17 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\nconsole.log(0)\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n/* harmony import */ var _tabs_home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./tabs/home */ \"./src/tabs/home.js\");\n\n\n\n(0,_tabs_home__WEBPACK_IMPORTED_MODULE_1__.sayHi)()\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/tabs/home.js":
+/*!**************************!*\
+  !*** ./src/tabs/home.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   sayHi: () => (/* binding */ sayHi)\n/* harmony export */ });\nconst sayHi = () => console.log(\"hi\");\n\n//# sourceURL=webpack://restaurant-page/./src/tabs/home.js?");
 
 /***/ })
 
