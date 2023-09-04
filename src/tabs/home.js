1d2cc5f370b184b4dd0c1e1dd41578c2cd22b38b
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-04 13:05:56 +0800

    created home page

--- src/tabs/home.js
+++ src/tabs/home.js
@@ -1,1 +1,68 @@
-export const sayHi = () => console.log("hi");
+export const displayHome= (() => {
+    const container = document.querySelector("#container");
+    
+    const header= document.createElement("div");
+    header.classList.add("header");
+    container.appendChild(header);
+
+    const homeTab= document.createElement("button");
+    homeTab.classList.add("home");
+    homeTab.innerText= "Home";
+    header.appendChild(homeTab);
+    const menuTab= document.createElement("button");
+    menuTab.classList.add("menu");
+    menuTab.innerText= "Menu";
+    header.appendChild(menuTab);
+    const contactTab= document.createElement("button");
+    contactTab.classList.add("contact");
+    contactTab.innerText= "Contact";
+    header.appendChild(contactTab);
+
+    const main= document.createElement("div");
+    main.classList.add("main");
+    container.appendChild(main);
+
+    const content=document.createElement("div");
+    content.classList.add("content");
+    main.appendChild(content);
+
+    const name= document.createElement("h1");
+    name.classList.add("name");
+    name.innerText= "Food Restaurant";
+    content.appendChild(name);
+
+    const descriptionBox= document.createElement("div");
+    descriptionBox.classList.add("descriptionBox");
+    content.appendChild(descriptionBox);
+    const title1= document.createElement("h2");
+    title1.innerText= "Description";
+    descriptionBox.appendChild(title1);
+    const description= document.createElement("div");
+    description.innerText= "This restaurant serves food! Our food, unlike most restaurants, can be eaten. Food eaten here will result in you eating food in this restaurant! The food in our restaurant will feed you food so you don't have to be hungry until the next time you need to eat food. Our food is made of 100% natural food. WOW!";
+    descriptionBox.appendChild(description);
+
+    const hoursBox= document.createElement("div");
+    hoursBox.classList.add("hoursBox");
+    content.appendChild(hoursBox);
+    const title2= document.createElement("h2");
+    title2.innerText= "Hours";
+    hoursBox.appendChild(title2);
+    const hours= document.createElement("div");
+    hours.innerText= "Sunday: 4am - 5am" + "\n" + "Monday: 4am - 5am" + "\n" + "Tuesday: 4am - 5am" + "\n" + "Wednesday: 4am - 5am" + "\n" + "Thursday: 4am - 5am" + "\n" + "Friday: 4am - 5am" + "\n" + "Saturday: 4am - 5am";
+    hoursBox.appendChild(hours);
+
+    const locationBox= document.createElement("div");
+    locationBox.classList.add("locationBox");
+    content.appendChild(locationBox);
+    const title3= document.createElement("h2");
+    title3.innerText="Location";
+    locationBox.appendChild(title3);
+    const location= document.createElement("div");
+    location.innerText= "123 Unknown Street, Knowhere, Mars";
+    locationBox.appendChild(location);
+
+    const footer= document.createElement("div");
+    footer.classList.add("footer");
+    container.appendChild(footer);
+    footer.innerText= "For The Odin Project";
+})();
